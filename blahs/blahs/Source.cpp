ec4f4354eae9f7e6d5f9b5b96e2f24209dba8546
parkerBurton <36173498+parkerBurton@users.noreply.github.com>
2018-02-21 00:44:12 -0700

    We've tried another change
    Woo Hoo

--- blahs/blahs/Source.cpp
+++ blahs/blahs/Source.cpp
@@ -4,5 +4,9 @@
 using namespace std;
 
 int main() {
-	cout << "This file is now different than my pre-GIT file" << endl;
+	cout << "I'm changing it again, and we'll do some math! \n\n" << endl;
+	int math = 0;
+	math = 4 + exp(math);
+	cout << "My answer should be 5, and it's..." << math << endl;
+	return 0;
 }
