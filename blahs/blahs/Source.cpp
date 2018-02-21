3ff1ddfc18acc6574115654b942f4a2596188f07
parkerBurton <36173498+parkerBurton@users.noreply.github.com>
2018-02-21 00:36:20 -0700

    I'm testing my GIT Skills
    I changed Source.cpp

--- blahs/blahs/Source.cpp
+++ blahs/blahs/Source.cpp
@@ -3,30 +3,6 @@
 
 using namespace std;
 
-class Poop {
-public:
-	int log;
-	string shiz;
-	Poop() {
-		log = 10;
-		shiz = "slop";
-	}
-
-};
-
 int main() {
-	/*Poop* pointer;
-	pointer = new Poop();
-	cout << pointer->log << endl;
-	Poop crap;
-	cout << crap.log << endl;
-	cout << crap.shiz << endl;*/
-
-	int poop = 7;
-	int* ptr = &poop;
-	cout << ptr << " " << *ptr << endl;
-	*ptr = 8;
-	cout << ptr << " " << *ptr << endl;
-	
-	return 0;
+	cout << "This file is now different than my pre-GIT file" << endl;
 }
